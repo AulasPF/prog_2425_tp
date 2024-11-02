abfdcc4f5af6583e7151b78a1d366eaccc406071
Pedro Fonseca <pf@ua.pt>
2024-11-02 18:34:53 +0000

    Aula 08: Commit inicial

--- Aula07/duaslinhas.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-  
-  char frase2linhas[]="Frase em\nduas linhas."; 
-  
-  printf("%s",frase2linhas); 
-
-}
--- Aula07/scanf.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-  char word[16];
-  scanf("%15s",word); 
-  printf("-%s-", word); 
-}
